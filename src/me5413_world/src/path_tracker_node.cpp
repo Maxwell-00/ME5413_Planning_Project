f4c252d04ee398c953bbb70b05dc0eaf7f606418
Maxwell-00 <147805053+Maxwell-00@users.noreply.github.com>
2024-03-22 22:42:12 +0800

    Update path_tracker_node.cpp

--- src/me5413_world/src/path_tracker_node.cpp
+++ src/me5413_world/src/path_tracker_node.cpp
@@ -1,129 +1,116 @@
-/** path_tracker_node.cpp
+/**
+ * @file path_tracker_node.cpp
+ * @brief Implementation of the PathTrackerNode class
  *
- * Copyright (C) 2024 Shuo SUN & Advanced Robotics Center, National University of Singapore
+ * This file contains the implementation of the PathTrackerNode class, which defines
+ * a ROS node responsible for controlling a robot to track a given path.
+ * It includes methods for subscribing to robot odometry and local path topics, computing control outputs,
+ * and publishing velocity commands.
  *
- * MIT License
- *
- * ROS Node for robot to track a given path
+ * @author Shuo SUN & Advanced Robotics Center, National University of Singapore
+ * @date 2024
+ * @license MIT License
  */
 
-#include "me5413_world/math_utils.hpp"
 #include "me5413_world/path_tracker_node.hpp"
 
 namespace me5413_world
 {
 
-// Dynamic Parameters
-double SPEED_TARGET;
-double PID_Kp, PID_Ki, PID_Kd;
-double STANLEY_K;
-bool PARAMS_UPDATED;
+    PathTrackerNode::PathTrackerNode() : nh_("~"), pure_pursuit_controller_(1.0)
+    {
+        nh_.param("lookahead_distance", lookahead_distance_, 1.0);
+        pure_pursuit_controller_.setLookaheadDistance(lookahead_distance_);
 
-void dynamicParamCallback(const me5413_world::path_trackerConfig& config, uint32_t level)
-{
-  // Common Params
-  SPEED_TARGET = config.speed_target;
-  // PID
-  PID_Kp = config.PID_Kp;
-  PID_Ki = config.PID_Ki;
-  PID_Kd = config.PID_Kd;
-  // Stanley
-  STANLEY_K = config.stanley_K;
+        sub_robot_odom_ = nh_.subscribe("/gazebo/ground_truth/state", 1, &PathTrackerNode::robotOdomCallback, this);
+        sub_local_path_ = nh_.subscribe("/me5413_world/planning/local_path", 1, &PathTrackerNode::localPathCallback, this);
+        pub_cmd_vel_ = nh_.advertise<geometry_msgs::Twist>("/jackal_velocity_controller/cmd_vel", 1);
+    };
 
-  PARAMS_UPDATED = true;
-}
+    void PathTrackerNode::localPathCallback(const nav_msgs::Path::ConstPtr& path)
+    {
+        local_path_ = path->poses;
 
-PathTrackerNode::PathTrackerNode() : tf2_listener_(tf2_buffer_)
-{
-  f = boost::bind(&dynamicParamCallback, _1, _2);
-  server.setCallback(f);
+        // Update lookahead distance in the PurePursuitController
+        pure_pursuit_controller_.setLookaheadDistance(lookahead_distance_);
 
-  this->sub_robot_odom_ = nh_.subscribe("/gazebo/ground_truth/state", 1, &PathTrackerNode::robotOdomCallback, this);
-  this->sub_local_path_ = nh_.subscribe("/me5413_world/planning/local_path", 1, &PathTrackerNode::localPathCallback, this);
-  this->pub_cmd_vel_ = nh_.advertise<geometry_msgs::Twist>("/jackal_velocity_controller/cmd_vel", 1);
+        // Compute control outputs using pure pursuit algorithm
+        geometry_msgs::Twist cmd_vel = computeControlOutputs(odom_world_robot_, local_path_);
 
-  // Initialization
-  this->robot_frame_ = "base_link";
-  this->world_frame_ = "world";
+        // Publish control command
+        pub_cmd_vel_.publish(cmd_vel);
+    }
 
-  this->pid_ = control::PID(0.1, 1.0, -1.0, PID_Kp, PID_Ki, PID_Kd);
-}
+    void PathTrackerNode::robotOdomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+    {
+        odom_world_robot_ = *odom;
+    }
 
-void PathTrackerNode::localPathCallback(const nav_msgs::Path::ConstPtr& path)
-{
-  // Calculate absolute errors (wrt to world frame)
-  this->pose_world_goal_ = path->poses[11].pose;
-  this->pub_cmd_vel_.publish(computeControlOutputs(this->odom_world_robot_, this->pose_world_goal_));
+    geometry_msgs::Twist PathTrackerNode::computeControlOutputs(const nav_msgs::Odometry& odom_robot, const std::vector<geometry_msgs::PoseStamped>& path)
+    {
+        geometry_msgs::Twist cmd_vel;
 
-  return;
-}
+        if (path.empty())
+        {
+            ROS_WARN("Local path is empty, cannot compute control outputs");
+            return cmd_vel;
+        }
 
-void PathTrackerNode::robotOdomCallback(const nav_msgs::Odometry::ConstPtr& odom)
-{
-  this->world_frame_ = odom->header.frame_id;
-  this->robot_frame_ = odom->child_frame_id;
-  this->odom_world_robot_ = *odom.get();
+        // Get current robot position and orientation
+        geometry_msgs::Pose robot_pose = odom_robot.pose.pose;
+        tf2::Vector3 robot_position(robot_pose.position.x, robot_pose.position.y, robot_pose.position.z);
+        tf2::Quaternion robot_orientation;
+        tf2::fromMsg(robot_pose.orientation, robot_orientation);
 
-  return;
-}
+        // Find the nearest point in the path
+        int nearest_idx = 0;
+        double min_distance = std::numeric_limits<double>::max();
+        for (int i = 0; i < path.size(); ++i)
+        {
+            double distance = tf2::tf2Distance(robot_position, tf2::Vector3(path[i].pose.position.x, path[i].pose.position.y, path[i].pose.position.z));
+            if (distance < min_distance)
+            {
+                nearest_idx = i;
+                min_distance = distance;
+            }
+        }
 
-double PathTrackerNode::computeStanelyControl(const double heading_error, const double cross_track_error, const double velocity)
-{
-  const double stanley_output = -1.0*(heading_error + std::atan2(STANLEY_K*cross_track_error, std::max(velocity, 0.3)));
+        // Find the lookahead point
+        int lookahead_idx = nearest_idx;
+        double total_distance = 0;
+        while (total_distance < lookahead_distance_ && lookahead_idx < path.size() - 1)
+        {
+            total_distance += tf2::tf2Distance(tf2::Vector3(path[lookahead_idx].pose.position.x, path[lookahead_idx].pose.position.y, path[lookahead_idx].pose.position.z),
+                                               tf2::Vector3(path[lookahead_idx + 1].pose.position.x, path[lookahead_idx + 1].pose.position.y, path[lookahead_idx + 1].pose.position.z));
+            lookahead_idx++;
+        }
 
-  return std::min(std::max(stanley_output, -2.2), 2.2);
-}
+        // Compute control outputs using pure pursuit algorithm
+        if (lookahead_idx < path.size())
+        {
+            double goal_x = path[lookahead_idx].pose.position.x;
+            double goal_y = path[lookahead_idx].pose.position.y;
 
-geometry_msgs::Twist PathTrackerNode::computeControlOutputs(const nav_msgs::Odometry& odom_robot, const geometry_msgs::Pose& pose_goal)
-{
-  // Heading Error
-  tf2::Quaternion q_robot, q_goal;
-  tf2::fromMsg(odom_robot.pose.pose.orientation, q_robot);
-  tf2::fromMsg(pose_goal.orientation, q_goal);
-  const tf2::Matrix3x3 m_robot = tf2::Matrix3x3(q_robot);
-  const tf2::Matrix3x3 m_goal = tf2::Matrix3x3(q_goal);
+            // Transform the goal point to the robot frame
+            tf2::Vector3 goal_in_robot_frame = tf2::quatRotate(robot_orientation.inverse(), tf2::Vector3(goal_x - robot_position.x(), goal_y - robot_position.y(), 0));
 
-  double roll, pitch, yaw_robot, yaw_goal;
-  m_robot.getRPY(roll, pitch, yaw_robot);
-  m_goal.getRPY(roll, pitch, yaw_goal);
+            // Calculate the steering angle
+            double steering_angle = atan2(goal_in_robot_frame.y(), goal_in_robot_frame.x());
 
-  const double heading_error = unifyAngleRange(yaw_robot - yaw_goal);
+            // Apply the steering angle to the control command
+            cmd_vel.angular.z = steering_angle;
+            cmd_vel.linear.x = 1.0; // Set linear velocity to a constant value for simplicity
+        }
 
-  // Lateral Error
-  tf2::Vector3 point_robot, point_goal;
-  tf2::fromMsg(odom_robot.pose.pose.position, point_robot);
-  tf2::fromMsg(pose_goal.position, point_goal);
-  const tf2::Vector3 V_goal_robot = point_robot - point_goal;
-  const double angle_goal_robot = std::atan2(V_goal_robot.getY(), V_goal_robot.getX());
-  const double angle_diff = angle_goal_robot - yaw_goal;
-  const double lat_error = V_goal_robot.length()*std::sin(angle_diff);
-
-  // Velocity
-  tf2::Vector3 robot_vel;
-  tf2::fromMsg(this->odom_world_robot_.twist.twist.linear, robot_vel);
-  const double velocity = robot_vel.length();
-
-  geometry_msgs::Twist cmd_vel;
-  if (PARAMS_UPDATED)
-  {
-    this->pid_.updateSettings(PID_Kp, PID_Ki, PID_Kd);
-    PARAMS_UPDATED = false;
-  }
-  cmd_vel.linear.x = this->pid_.calculate(SPEED_TARGET, velocity);
-  cmd_vel.angular.z = computeStanelyControl(heading_error, lat_error, velocity);
-
-  // std::cout << "robot velocity is " << velocity << " throttle is " << cmd_vel.linear.x << std::endl;
-  // std::cout << "lateral error is " << lat_error << " heading_error is " << heading_error << " steering is " << cmd_vel.angular.z << std::endl;
-
-  return cmd_vel;
-}
+        return cmd_vel;
+    }
 
 } // namespace me5413_world
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "path_tracker_node");
-  me5413_world::PathTrackerNode path_tracker_node;
-  ros::spin();  // spin the ros node.
-  return 0;
+    ros::init(argc, argv, "path_tracker_node");
+    me5413_world::PathTrackerNode path_tracker_node;
+    ros::spin(); // spin the ros node.
+    return 0;
 }
